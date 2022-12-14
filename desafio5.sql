e2c480f051728bbc7e6eeac66655991072a5e073
Felipe <felipemalentaq@hotmail.com>
2022-12-14 19:00:43 -0300

    requisito 5 ok

--- desafio5.sql
+++ desafio5.sql
@@ -2,9 +2,8 @@ SELECT
 	c.cancoes AS cancao,
     COUNT(h.cancoes_id) AS reproducoes
 FROM SpotifyClone.cancoes c
-INNER JOIN historico_de_reproducoes h
+INNER JOIN SpotifyClone.historico_de_reproducoes h
 ON c.cancoes_id = h.cancoes_id
-WHERE h.cancoes_id <> 9
 GROUP BY h.cancoes_id
-ORDER BY h.cancoes_id DESC, c.cancoes
+ORDER BY `reproducoes` DESC, c.cancoes
 LIMIT 2;
